1f715b6558a45da696e4caa7c0e68a1433da4908
Amer Saleh <amerysaleh@yahoo.com>
2017-04-21 23:44:20 -0400

    Replacing http by https for api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,7 +41,7 @@ window.onload = function() {
      		var animal = $(this).attr("data-animal");
 
      		// Constructing a queryURL using the animal name
-      		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
        		animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       		// Performing an AJAX request with the queryURL
